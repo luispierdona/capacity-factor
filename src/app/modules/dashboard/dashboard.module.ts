364d78e99afd68a4fd282f612adf432b4071cbf8
Luis Matias Pierdona <luismathias.pierdona@de.adecco.net>
2022-09-22 20:20:40 +0200

    Dashboard component added

---
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DashboardComponent,
+  },
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+  ],
+  declarations: [
+    DashboardComponent
+  ],
+  exports: [],
+  providers: [],
+})
+export class DashboardPageModule {}
